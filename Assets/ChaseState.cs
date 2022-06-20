86c92df1243e96f6c99a3b1c8a8267a6824801c6
mrcunl <rvonxzz@gmail.com>
2022-06-20 14:12:29 +0300

    Zombie Anm
    zombie animasyonları eklendi

---
+++ Assets/ChaseState.cs
@@ -0,0 +1,46 @@
+using UnityEngine.AI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : StateMachineBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent.speed = 0.5f;
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(player.position);
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        if (distance > 15)
+            animator.SetBool("Chasing", false);
+        if (distance < 1.7f)
+            animator.SetBool("Attacking", true);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(animator.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/WalkPatrol1.cs
@@ -0,0 +1,46 @@
+using UnityEngine.AI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPatrol1 : StateMachineBehaviour
+{
+    float timer;
+    List<Transform> wayPoints1 = new List<Transform>();
+    NavMeshAgent agent;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        GameObject go1 = GameObject.FindGameObjectWithTag("Waypoints1");
+        foreach (Transform t in go1.transform)
+            wayPoints1.Add(t);
+
+        agent.SetDestination(wayPoints1[Random.Range(0, wayPoints1.Count)].position);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+            agent.SetDestination(wayPoints1[Random.Range(0, wayPoints1.Count)].position);
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Implement code that processes and affects root motion
+    }
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Implement code that sets up animation IK (inverse kinematics)
+    }
+}
